c9e859a7abd67c4a64f471f2c49f090fe7bd341f
subhankar rath <subhankarrath07@gmail.com>
2026-01-26 20:31:05 +0530

    sum of the digits of the number

---
+++ SumOfDigits.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static int sumDigit(int n){
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum+=rem;
+            n/=10;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        System.out.println("sum of digits"+sumDigit(n));
+
+    }
+}
