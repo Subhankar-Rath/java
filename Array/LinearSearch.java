208353c9fbf66cc92cd697d25e67dc9155579de1
subhankar rath <subhankarrath07@gmail.com>
2026-02-04 09:49:05 +0530

    linear search

---
+++ Array/LinearSearch.java
@@ -0,0 +1,23 @@
+package Array;
+public class LinearSearch{
+    public static int linearsearch(int numbers[],int key){
+        for(int i=0;i<=numbers.length;i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6};
+        int key=5;
+        int index=linearsearch(numbers, key);
+        if(index==-1){
+            System.out.println("not found");
+
+        }
+        else{
+            System.out.println("the index is" +index);
+        }
+    }
+}
