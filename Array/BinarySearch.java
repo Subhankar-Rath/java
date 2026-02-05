e654e52aeadd08383b5ee06ff31d114338431e8c
subhankar rath <subhankarrath07@gmail.com>
2026-02-05 20:10:44 +0530

    binary search of time complexity O(log n)

---
+++ Array/BinarySearch.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class BinarySearch {
+    public static int binarySearching(int numbers[],int key){
+        int start=0,end=numbers.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(numbers[mid]>key){
+                start=mid+1;
+            }
+            if(numbers[mid]==key){
+                return mid;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6,7};
+        int key=4;
+        System.out.println("the index is "+binarySearching(numbers, key));
+    }
+}
