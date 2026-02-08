d68c0215ce4777ad2f07f0b886483b41353d5a78
subhankar rath <subhankarrath07@gmail.com>
2026-02-08 09:03:31 +0530

    largest index

--- practice-problems/LargestIndex.java
+++ practice-problems/LargestIndex.java
@@ -1,18 +1,18 @@
+
+
 public class LargestIndex {
-
-    public static int largestNo(int number[]) {
-        int largest = Integer.MIN_VALUE;
-
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] > largest) {   // FIXED
-                largest = number[i];
+    public static int largestNo(int number[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<number.length;i++){
+            if(number[i]>largest){
+                largest=number[i];
             }
         }
-        return largest;   // FIXED
+        return largest;
     }
-
-    public static void main(String[] args) {
-        int number[] = {1, 2, 3, 4, 5};
-        System.out.println("Largest = " + largestNo(number));
+    public static void main(final String[] args) {
+        int number[]={1,2,3,4,5};
+        System.out.println("largest"+largestNo(number));
+        
     }
 }
