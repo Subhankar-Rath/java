4677e2a7e13dd18829f6eef8cfe7834a4ea37647
subhankar rath <subhankarrath07@gmail.com>
2026-02-12 10:04:28 +0530

    sub array using prefix

--- Array/SubArray.java
+++ Array/SubArray.java
@@ -4,7 +4,7 @@ public class SubArray {
     public static void subArray(int number[]){
         for(int i=0;i<number.length;i++){
             for(int j=i;j<number.length;j++){
-                for(int k=1;k<=j;k++){
+                for(int k=i;k<=j;k++){
                     System.out.print(number[k]+" ");
                 }
                 System.out.println();
