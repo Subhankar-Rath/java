83630971c1960be021da7ce74f5991e91227e0ae
subhankar rath <subhankarrath07@gmail.com>
2026-03-27 22:56:25 +0530

    frequency of number in array

---
+++ 2d-Array/assignment/Frequency.java
@@ -0,0 +1,20 @@
+package assignment;
+
+public class Frequency {
+    public static int frequency(int matrix[][],int key){
+        int count=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==key){
+                    count++;
+            }
+        }
+    }
+    return count;
+}
+    public static void main(String args[]){
+        int matrix[][]={{4,7,8},{8,8,7}};
+        int key=7;
+        System.out.println(frequency(matrix,key));
+    }
+}
