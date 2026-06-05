9cda78fb4a13c3d87bea3a159e1b225e98648216
subhankar rath <subhankarrath07@gmail.com>
2026-06-05 17:27:31 +0530

    revision

--- revision/r5.java
+++ revision/r5.java
@@ -1,5 +1,18 @@
 package revision;
 import java.util.*;
 public class r5 {
-    public static void main(String args[])
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+       char target = sc.next().charAt(0);
+        String result="";
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch!=target){
+                result+=ch;
+            }
+        }
+        System.err.println(result); 
+
+    }
 }
