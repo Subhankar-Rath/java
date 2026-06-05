ff6d5a120718ff78a0370d6ad47b3fd8e7976975
subhankar rath <subhankarrath07@gmail.com>
2026-06-05 17:49:33 +0530

    revision

---
+++ revision/r6.java
@@ -0,0 +1,14 @@
+package revision;
+import java.util.*;
+public class r6{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            System.out.println(ch);
+        }
+
+
+    }
+}
