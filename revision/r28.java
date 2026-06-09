0062111310b52684b7f86b2f28976b667616c5f2
subhankar rath <subhankarrath07@gmail.com>
2026-06-09 22:04:14 +0530

    revision

---
+++ revision/r28.java
@@ -0,0 +1,15 @@
+public class r28 {
+    public static void main(String args[]){
+        int target=510;
+        int arr[]={501, 503, 507, 510, 515};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                System.out.println(i);
+            }
+            
+            
+        }
+        System.out.println("not in the list");
+    }
+    
+}
