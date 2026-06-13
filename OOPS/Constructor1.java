27f773e5016b25babf4ffbfbef98669a618bc87d
subhankar rath <subhankarrath07@gmail.com>
2026-06-13 19:54:04 +0530

    constructor

---
+++ OOPS/Constructor1.java
@@ -0,0 +1,16 @@
+package OOPS;
+
+public class Constructor1 {
+    public static void main(String a[]){
+        Student s1=new Student();
+    }
+
+}
+class Student{
+    String name;
+    int roll;
+
+    Student(){
+        System.out.println("i am subhankar Rath");
+    }
+}
