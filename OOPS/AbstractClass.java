249a748b3436eccc2021a9265bebb1ec66682741
subhankar rath <subhankarrath07@gmail.com>
2026-06-17 23:27:58 +0530

    abstract class

---
+++ OOPS/AbstractClass.java
@@ -0,0 +1,30 @@
+package OOPS;
+
+public class AbstractClass {
+    public static void main(String args[]){
+        Horse h=new Horse();
+        h.eat();
+        h.walk();
+        Chicken c=new Chicken();
+        c.eat();
+        c.walk();
+    }
+}
+
+abstract class Animal{
+    void eat(){
+        System.out .println("eating");
+    }
+    abstract void walk();
+}
+class Horse extends Animal{
+    void walk(){
+        System.out.println("4 legs");
+    }
+}
+class Chicken extends Animal{
+    void walk(){
+        System.out.println("2 legs");
+    }
+}
+
--- OOPS/OverRiding.java
+++ OOPS/OverRiding.java
@@ -14,6 +14,7 @@ class Animal{
     }
 }
 class Deer extends Animal{
+    @Override
     void eat(){
         System.out.println("eating grass");
     }
