8f54f388c7f2ac3310f6eec808f470fc0105cd1b
subhankar rath <subhankarrath07@gmail.com>
2026-07-29 19:21:29 +0530

    printing the 1 to n(recursion)

---
+++ Recursion/Problem2.java
@@ -0,0 +1,15 @@
+public class Problem2 {
+    public static void printInc(int n){
+        if(n==1){
+            System.out.println(n+" ");
+
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n+" ");
+    }
+    public static void main(String a[]){
+        int n=10;
+        printInc(n);
+    }
+}
